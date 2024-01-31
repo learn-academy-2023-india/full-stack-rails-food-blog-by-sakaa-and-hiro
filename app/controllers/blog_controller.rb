368da965a21ad48dd824227b11c2f9baff06f7e1
Yoshihiro Yamada <yoshihiro.yamada1995@gmail.com>
2024-01-31 09:42:09 -0800

    finished story 2 with Sakaa, Jimmy, Hiro

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,4 +2,7 @@ class BlogController < ApplicationController
     def index
         @blogs = Blog.all 
     end
+    def show 
+        @one_blog = Blog.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/blogs', to: 'blog#index', as: 'blogs'
+  get '/blogs/:id', to: 'blog#show', as: 'show'
 end
