5a73560bc39f528b487d58467d3dc887889d8f02
Yoshihiro Yamada <yoshihiro.yamada1995@gmail.com>
2024-01-31 10:26:11 -0800

    Finished controller and routes for create with Sakaa, Jimmy, Hiro

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -5,4 +5,18 @@ class BlogController < ApplicationController
     def show 
         @one_blog = Blog.find(params[:id])
     end
+    def new
+        @new_blog = Blog.new
+    end
+    def create
+        @new_blog = Blog.create(blog_params)
+        if @new_blog.valid?
+            redirect_to blogs_path
+        end
+    end
+
+    private
+    def blog_params
+        params.require(:blog).permit(:title, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/blogs', to: 'blog#index', as: 'blogs'
+  get '/blogs/new', to: 'blog#new', as: 'new'
+  post '/blogs', to: 'blog#create'
   get '/blogs/:id', to: 'blog#show', as: 'show'
 end
