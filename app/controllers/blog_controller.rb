2659e7c392c764ba963e4a7a979f777de6158e0f
Yoshihiro Yamada <yoshihiro.yamada1995@gmail.com>
2024-01-31 11:23:17 -0800

    finished edit with Sakaa, Jimmy, Hiro

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,9 +2,6 @@ class BlogController < ApplicationController
     def index
         @blogs = Blog.all 
     end
-    def show 
-        @one_blog = Blog.find(params[:id])
-    end
     def new
         @new_blog = Blog.new
     end
@@ -13,6 +10,12 @@ class BlogController < ApplicationController
         if @new_blog.valid?
             redirect_to blogs_path
         end
+    end
+    def show 
+        @one_blog = Blog.find(params[:id])
+    end
+    def edit
+        @modified_blog = Blog.find(params[:id])
     end
     def delete
         @no_blog = Blog.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get '/blogs', to: 'blog#index', as: 'blogs'
   get '/blogs/new', to: 'blog#new', as: 'new'
   post '/blogs', to: 'blog#create'
-  get '/blogs/:id', to: 'blog#show', as: 'show'
+  get '/blogs/:id', to: 'blog#show', as: 'blog'
+  get '/blogs/:id/edit', to: 'blog#edit', as: 'edit'
+  patch '/blogs/:id', to: 'blog#update'
   delete '/blogs/:id', to: 'blog#delete', as: 'delete'
+
 end
